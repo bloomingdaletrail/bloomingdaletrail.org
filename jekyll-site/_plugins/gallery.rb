044274ef87c53a06ce1b68a11e68b87bc98dbbe1
Paul Smith <paulsmith@pobox.com>
2015-06-26 01:19:27 -0500

    WIP

---
+++ jekyll-site/_plugins/gallery.rb
@@ -0,0 +1,53 @@
+module Jekyll
+    class GalleryPage < Page
+        def initialize(site, base, dir, gallery, galleries)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'gallery_page.html')
+            self.data['gallery'] = gallery
+        end
+    end
+
+    class GalleryPageGenerator < Generator
+        safe true
+
+        def generate(site)
+            if site.layouts.key? 'gallery_page'
+                data = site.config['gallery_data_name']
+                gallery_data = site.data[data]
+                gallery_data['galleries'].each do |gallery|
+                    dir = File.join(gallery_data['root_dir'], gallery['slug'])
+                    site.pages << GalleryPage.new(site, site.source, dir, gallery, galleries)
+                end
+            end
+        end
+    end
+
+    class GalleryIndexPage < Page
+        def initialize(site, base, dir, gallery_data)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
+
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'gallery_index.html')
+            self.data['galleries'] = gallery_data
+        end
+    end
+
+    class GalleryIndexPageGenerator < Generator
+        safe true
+
+        def generate(site)
+            if site.layouts.key? 'gallery_index'
+                data = site.config['gallery_data_name']
+                gallery_data = site.data[data]
+                site.pages << GalleryIndexPage.new(site, site.source, gallery_data['root_dir'], gallery_data)
+            end
+        end
+    end
+end
